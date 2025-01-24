cab9a7bd1d64bcb455c24ac94ad13d6a4901bba0
YoungminKim <ggred0123@gmail.com>
2025-01-24 15:16:26 +0900

    swaggercheck

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, InternalServerErrorException } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller()
-@ApiTags('Eventory')
+@ApiTags("KAIST Map")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('health')
-  @ApiOperation({ summary: 'Health check' })
+  @Get("health")
+  @ApiOperation({ summary: "Health check" })
   getHello(): string {
     return this.appService.healthCheck();
   }
